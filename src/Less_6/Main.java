b93749d782da2cca4fe4f8c0e240705e64db03ce
Margarita Medvedeva <margaret_ne@rambler.ru>
2020-01-20 16:12:51 +0300

    Урок 6

--- src/Less_6/Main.java
+++ src/Less_6/Main.java
@@ -1,8 +1,6 @@
 package Less_6;
 
-/**
- * Created by mma on 20.01.2020.
- */
+
 public class Main {
     public static void main(String[] args) {
         MyTreeMap<Integer, String> map = new MyTreeMap<>();
@@ -13,10 +11,13 @@ public class Main {
         map.put(2,"two");
         map.put(4,"four");
 
+
         System.out.println(map.get(1));
         System.out.println(map.get(2));
         map.delete(2);
+        map.delete(3);
         System.out.println(map.contains(2));
+        System.out.println(map.contains(1));
 
         System.out.println(map);
 
